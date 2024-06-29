84f9eb57960d955ef8463e91de0b7bb722e2927a
Abdul Wajid <wajid.jmi@gmail.com>
2024-06-29 17:34:08 +0530

    testing issue fix

--- components/Chat/MobileChat/index.tsx
+++ components/Chat/MobileChat/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import cx from 'classnames';
-import ReactMarkdown from 'react-markdown';
+// import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
 import Typing from 'react-typing-animation';
 import { toast, ToastContainer } from 'react-toastify';
@@ -22,15 +22,15 @@ import Delete from '@public/Images/svgs/delete.svg';
 // import InActiveOption from '@public/Images/svgs/inactive-option.svg';
 
 import LoadingDots from '@components/ui/LoadingDots';
-import { Accordion, AccordionContent, AccordionItem } from '@components/ui/accordion';
+// import { Accordion, AccordionContent, AccordionItem } from '@components/ui/accordion';
 import ShimmerUiContainer from '@components/ShimmerContainer';
 import UploadPDFModal from '@/components/UploadPDFModal';
 import { APP_URL, ApiRoute } from '@/components/appConstant';
 import DeletePdfModal from '@/components/DeletePdfModal';
-
-import { useRouter } from 'next/router';
-import styles from './styles.module.scss';
 import { renderMessageContent } from '@/utils/helper';
+import { useRouter } from 'next/router';
+
+import styles from './styles.module.scss';
 
 const MobileChat = () => {
     const [query, setQuery] = useState<string>('');
@@ -66,7 +66,7 @@ const MobileChat = () => {
     const [selectedPdf, setSelectedPdf] = useState('');
     const [pdfOptions, setPdfOptions] = useState([]);
     const [selectedPdfCard, setSelectedPdfCard] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+    // const [isLoading, setIsLoading] = useState(true);
     const [hamburger, setHamburger] = useState(false);
     const [, setPreviousUserText] = useState('');
     const [dropdownVisible, setDropdownVisible] = useState(false);
@@ -166,7 +166,7 @@ const MobileChat = () => {
 
         const requestBody: any = {
             question,
-            history,
+            // history,
             filename: selectedValue || `${selectedPdf}`,
         };
 
@@ -580,7 +580,7 @@ const MobileChat = () => {
 
                                 return null;
                             })}
-                            <div className='p-5'>
+                            {/* <div className='p-5'>
                                 {messageState.messages.map((msg, index) => (
                                     <div
                                         key={`SourceDocs-${index}`}
@@ -597,7 +597,7 @@ const MobileChat = () => {
                                         </Accordion>
                                     </div>
                                 ))}
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                 </div>
